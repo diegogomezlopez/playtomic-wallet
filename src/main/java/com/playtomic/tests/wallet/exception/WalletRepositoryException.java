20c811df8ac0dde93468336ba7dcd22e2b0594f4
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-08-10 12:25:54 +0200

    ADD WalletRepositoryException

---
+++ src/main/java/com/playtomic/tests/wallet/exception/WalletRepositoryException.java
@@ -0,0 +1,8 @@
+package com.playtomic.tests.wallet.exception;
+
+public class WalletRepositoryException extends RuntimeException {
+
+    public WalletRepositoryException(final String message) {
+        super(message);
+    }
+}
