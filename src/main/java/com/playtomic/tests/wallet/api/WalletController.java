e0bc20552648b3c79f59825116c6ebd5fa223f1b
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-08-12 22:33:14 +0200

    ADD WalletController

--- src/main/java/com/playtomic/tests/wallet/api/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/api/WalletController.java
@@ -1,16 +1,55 @@
 package com.playtomic.tests.wallet.api;
 
+import com.playtomic.tests.wallet.dto.RechargeRequest;
+import com.playtomic.tests.wallet.dto.ChargeRequest;
+import com.playtomic.tests.wallet.dto.WalletDTO;
+import com.playtomic.tests.wallet.service.BalanceService;
+import com.playtomic.tests.wallet.service.WalletService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/wallet")
 public class WalletController {
     private Logger log = LoggerFactory.getLogger(WalletController.class);
 
-    @RequestMapping("/")
+    private final WalletService walletService;
+    private final BalanceService balanceService;
+
+    public WalletController(final WalletService walletService, final BalanceService balanceService) {
+        this.walletService = walletService;
+        this.balanceService = balanceService;
+    }
+
+    @RequestMapping({"", "/"})
     void log() {
         log.info("Logging from /");
+    }
+
+    @GetMapping("/{id}")
+    public WalletDTO getWallet(final @PathVariable Long id) {
+        return walletService.getWallet(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createWallet(final @RequestBody WalletDTO walletDTO) {
+        walletService.createWallet(walletDTO);
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping("/recharge")
+    public void recharge(final @RequestBody RechargeRequest request) {
+        balanceService.recharge(request.getWalletId(), request.getCreditCardNumber(), request.getAmount());
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping("/charge")
+    public void charge(final @RequestBody ChargeRequest request) {
+        balanceService.charge(request.getWalletId(), request.getAmount());
     }
 }
