af628fb7913510a8b2cd22411d5b05d8cf7b5610
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-08-12 22:31:26 +0200

    ADD WalletRechargeException

---
+++ src/main/java/com/playtomic/tests/wallet/exception/WalletRechargeException.java
@@ -0,0 +1,8 @@
+package com.playtomic.tests.wallet.exception;
+
+public class WalletRechargeException extends RuntimeException {
+
+    public WalletRechargeException(final String message) {
+        super(message);
+    }
+}
