6c810f41411d8fdaeeb64f06e9ace47b619ed828
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-09-01 01:25:06 +0200

    UPDATE implements serializable

--- src/main/java/com/playtomic/tests/wallet/domain/Wallet.java
+++ src/main/java/com/playtomic/tests/wallet/domain/Wallet.java
@@ -1,6 +1,9 @@
 package com.playtomic.tests.wallet.domain;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
