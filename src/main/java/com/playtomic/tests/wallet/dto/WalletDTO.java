67d5b09b12ceb61e5e8a1b05cfb01c7ab4d64c56
diegogomezlopez <diegogomezlopez91@gmail.com>
2021-09-01 01:25:21 +0200

    UPDATE implements serializable

--- src/main/java/com/playtomic/tests/wallet/dto/WalletDTO.java
+++ src/main/java/com/playtomic/tests/wallet/dto/WalletDTO.java
@@ -5,13 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class WalletDTO {
+public class WalletDTO implements Serializable {
 
     private Long id;
     private BigDecimal balance;
